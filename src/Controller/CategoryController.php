d0767ce7fd026a93c3b5548a2bf6e110061beb46
Kacper <kacperzimmer@outlook.com>
2024-08-24 20:41:29 +0200

    deleting categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,8 +1,42 @@
 <?php
+// src/Controller/CategoryController.php
 
 namespace App\Controller;
 
-class CategoryController
-{
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/categories", name="category_list")
+     */
+    public function list(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/list.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+    /**
+     * @Route("/category/{id}/delete", name="category_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete-category' . $category->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Kategoria została usunięta pomyślnie.');
+        }
+
+        return $this->redirectToRoute('category_list');
+    }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -14,12 +14,27 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: Types::ARRAY, nullable: true)]
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::JSON, nullable: true)]
     private ?array $categories = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
     }
 
     public function getCategories(): ?array
